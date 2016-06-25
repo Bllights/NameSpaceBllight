8fbe8fb93d19f8c00ffd2a26888881635283b706
Bllights <ho2006@hanmail.net>
2016-06-25 22:27:51 +0900

    #3
    BLSprite Start2

--- BLSprite/BLSprite.cpp
+++ BLSprite/BLSprite.cpp
@@ -1,13 +1,21 @@
 #include "BLSPrite.h"
 namespace Bllight{
-void BLSPrite::SetAnimationImage(Texture2D* image[]){
-    int tmp=0;
-    for(Texture2D* img : image){
-        Animation[tmp++]=img;
+    void BLSPrite::SetAnimationImage(Texture2D* image[]){
+        int tmp=0;
+        for(Texture2D* img : image){
+            Animation[tmp++]=img;
+        }
+        Animation_Number=tmp;
     }
-    Animation_Number=tmp;
-}
 
+    Bllight* BLSPrite::Creat(std::string filepath){
+        Scale=1;
+        
+        return __super::create(filepath);
+    }
 
-
+    Bllight* BLSPrite::Creat(std::string filepath,int ZOrder){
+        Scale=1;
+        return __super::create(filepath,ZOrder);
+    }
 }
--- BLSprite/BLSprite.h
+++ BLSprite/BLSprite.h
@@ -7,13 +7,16 @@ USING_NS_CC;
 namespace Bllight{
     class BLSPrite : public Sprite{
     private:
+        typedef Sprite super;
         Texture2D* Animation[10];
         int Animation_Number;
 
+        int Scale;//1
     public
         void SetAnimationImage(Texture2D* image[]);
         int GetAnimationNum(){return Animation_Number;}
 
-
+        Bllight* Create(std::string filepath);
+        Bllight* Create(std::string filepath,int ZOrder);
     };
 }
